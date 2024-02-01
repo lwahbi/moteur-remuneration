0d50e025f8de3f678133afb3899934d2c86463e6
lotfi wahbi <lwahbi@gmail.com>
2024-02-01 22:46:02 +0100

    Initial DUMP

--- src/main/java/ma/globalperformance/app/MoteurRemunerationApplication.java
+++ src/main/java/ma/globalperformance/app/MoteurRemunerationApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableBatchProcessing
-@ComponentScan({"ma.globalperformance.*"})
 public class MoteurRemunerationApplication {
 
 	public static void main(String[] args) {
