6f7b386299d37eaa9d69ba32129bba762dc183f7
Rahali Hicham <rahali.hicham@gmail.com>
2024-03-13 21:37:36 +0000

    add some  config

--- src/main/java/ma/globalperformance/config/JobConfig.java
+++ src/main/java/ma/globalperformance/config/JobConfig.java
@@ -1,10 +1,9 @@
 package ma.globalperformance.config;
 
-import java.util.HashMap;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
+import ma.globalperformance.entity.ClientTransaction;
+import ma.globalperformance.entity.Remuneration;
+import ma.globalperformance.partitioner.CodeEsPartitioner;
+import ma.globalperformance.processor.RemunerationItemProcessor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -24,32 +23,30 @@ import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
-import ma.globalperformance.entity.ClientTransaction;
-import ma.globalperformance.entity.Remuneration;
-import ma.globalperformance.partitioner.CodeEsPartitioner;
-import ma.globalperformance.processor.RemunerationItemProcessor;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.HashMap;
 
 @Configuration
 public class JobConfig {
 
-	@Autowired
-	@Qualifier("datasource")
-	private DataSource datasource;
+    @Autowired
+    @Qualifier("datasource")
+    private DataSource datasource;
 
-	private final JobBuilderFactory jobBuilderFactory;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
-	private final StepBuilderFactory stepBuilderFactory;
-	
-	@Autowired
-	@Qualifier("entityManagerFactory")
-	private EntityManagerFactory entityManagerFactory;
-	
-	@Autowired
+    @Autowired
+    @Qualifier("entityManagerFactory")
+    private EntityManagerFactory entityManagerFactory;
+
+    @Autowired
     private CodeEsPartitioner codeEsPartitioner;
-	
-	@Autowired
-	private RemunerationItemProcessor remunerationItemProcessor ;
-	
+
+    @Autowired
+    private RemunerationItemProcessor remunerationItemProcessor;
+
     @Autowired
     private JpaPagingItemReader<ClientTransaction> reader;
 
@@ -57,7 +54,7 @@ public class JobConfig {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
     }
-    
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplateBuilder().build();
@@ -66,22 +63,30 @@ public class JobConfig {
     @Bean
     public TaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(6); // Nombre de threads à exécuter en parallèle
+        executor.setCorePoolSize(4); // Nombre de threads à exécuter en parallèle
         executor.setMaxPoolSize(500);
-        executor.setQueueCapacity(300);
+        executor.setThreadNamePrefix("remuneration-chunk-executor-");
+        executor.setAllowCoreThreadTimeOut(true);
+        executor.setKeepAliveSeconds(30);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(30);
+        executor.setRejectedExecutionHandler(new java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setThreadPriority(Thread.MAX_PRIORITY);
+        executor.setQueueCapacity(100);
         executor.initialize();
         return executor;
+
     }
 
-	@Bean
-	public Job chunkJob() throws Exception {
-		return jobBuilderFactory.get("Remuneration Chunk Job ...")
-				.incrementer(new RunIdIncrementer())
-				.start(masterStep())
-				.build();
-	}
-	
-	@Bean
+    @Bean
+    public Job chunkJob() throws Exception {
+        return jobBuilderFactory.get("Remuneration Chunk Job ...")
+                .incrementer(new RunIdIncrementer())
+                .start(masterStep())
+                .build();
+    }
+
+    @Bean
     public Step masterStep() throws Exception {
         return stepBuilderFactory.get("masterStep")
                 .partitioner(reumerationChunkStep().getName(), codeEsPartitioner)
@@ -90,31 +95,34 @@ public class JobConfig {
                 .taskExecutor(taskExecutor()) // Utiliser le TaskExecutor pour l'exécution parallèle
                 .build();
     }
-	
-	public Step reumerationChunkStep() throws Exception {
-		return stepBuilderFactory.get("reumerationChunkStep")
-				.<ClientTransaction, Remuneration>chunk(600)
-				.reader(reader)
-				.processor(remunerationItemProcessor)
-				.writer(jpaItemWriter())
-				//.listener(new RemunerationStepExecutionListener())
-				//.transactionManager(jpaTransactionManager)
-				.build();
-	}
-	
-	
+
+    public Step reumerationChunkStep() throws Exception {
+        return stepBuilderFactory.get("reumerationChunkStep")
+                .<ClientTransaction, Remuneration>chunk(600)
+                .reader(reader)
+                //.reader(jpaPagingItemReader(0, 100))
+                .processor(remunerationItemProcessor)
+                .writer(jpaItemWriter())
+                //.listener(new RemunerationStepExecutionListener())
+                //.transactionManager(jpaTransactionManager)
+                .build();
+    }
+
+
     @Bean(destroyMethod = "") // http://stackoverflow.com/a/23089536
     @StepScope
-    public JpaPagingItemReader<ClientTransaction> jpaPagingItemReader (@Value("#{stepExecutionContext[from]}") Integer from,
-                                               @Value("#{stepExecutionContext[to]}") Integer to
+    public JpaPagingItemReader<ClientTransaction> jpaPagingItemReader(@Value("#{stepExecutionContext[from]}") Integer from,
+                                                                      @Value("#{stepExecutionContext[to]}") Integer to
     ) {
-    	JpaPagingItemReader<ClientTransaction> reader = new JpaPagingItemReader<ClientTransaction>();
+        // whrite a sytem.out.println whith color to see if this method is called
+        System.out.println("\u001B[32m" + "jpaPagingItemReader" + "\u001B[0m");
+        JpaPagingItemReader<ClientTransaction> reader = new JpaPagingItemReader<ClientTransaction>();
         reader.setEntityManagerFactory(entityManagerFactory);
         reader.setSaveState(false);
 
         reader.setQueryString("SELECT clt " +
-                              "FROM ClientTransaction AS clt " +
-                              "WHERE clt.id BETWEEN :from AND :to");
+                "FROM ClientTransaction AS clt " +
+                "WHERE clt.id BETWEEN :from AND :to");
         reader.setParameterValues(new HashMap<String, Object>() {
             {
                 put("from", from.longValue());
@@ -124,13 +132,14 @@ public class JobConfig {
 
         return reader;
     }
-	
-	public ItemWriter<Remuneration> jpaItemWriter() {
-		JpaItemWriter<Remuneration> jpaItemWriter = 
-				new JpaItemWriter<Remuneration>();
-		
-		jpaItemWriter.setEntityManagerFactory(entityManagerFactory);
-		
-		return jpaItemWriter;
-	}
+
+    public ItemWriter<Remuneration> jpaItemWriter() {
+        System.out.println("jpaItemWriter");
+        JpaItemWriter<Remuneration> jpaItemWriter =
+                new JpaItemWriter<Remuneration>();
+
+        jpaItemWriter.setEntityManagerFactory(entityManagerFactory);
+
+        return jpaItemWriter;
+    }
 }
