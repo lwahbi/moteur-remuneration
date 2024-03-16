cf7bfe46fc7b664d1515831d3a08837d4eec31e6
Rahali Hicham <rahali.hicham@gmail.com>
2024-03-16 23:30:34 +0000

    add fields

---
+++ src/main/java/ma/globalperformance/batch/BatcherCalculator.java
@@ -0,0 +1,12 @@
+package ma.globalperformance.batch;
+
+public class BatcherCalculator {
+    public static void main(String[] args) {
+
+      //  /api/v1/paliers/findall
+
+        //select from table ClientTransaction to calculate  ;
+        System.out.println("Hello World!");
+
+    }
+}
