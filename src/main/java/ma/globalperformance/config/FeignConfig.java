b8035fd05f7e20903aba4f52012b8f208835b095
lwahbi <lwahbi@gmail.com>
2024-04-01 01:40:15 +0200

    Suppression FeignConfig

--- src/main/java/ma/globalperformance/config/FeignConfig.java
+++ src/main/java/ma/globalperformance/config/FeignConfig.java
@@ -1,15 +1,5 @@
 package ma.globalperformance.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.FeignClientFactoryBean;
-import org.springframework.cloud.openfeign.support.SpringEncoder;
-import org.springframework.cloud.openfeign.support.SpringMvcContract;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
 public class FeignConfig {
 
 //    @Autowired
--- src/main/java/ma/globalperformance/processor/PalierServiceClient.java
+++ src/main/java/ma/globalperformance/processor/PalierServiceClient.java
@@ -1,13 +1,5 @@
 package ma.globalperformance.processor;
 
-import java.util.List;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import ma.globalperformance.dto.PalierDTO;
-
 //@FeignClient(name = "palier-service", url = "${palier.service.url}")
 public interface PalierServiceClient {
 
